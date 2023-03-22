148c5226d1fb9fd93cb93f111c35f61853432aab
Tomsticks <miracleolaniyan@yahoo.com>
2023-03-22 04:06:08 -0700

    balal

---
+++ src/components/AboutUs.vue
@@ -0,0 +1,106 @@
+<template>
+    <section id="about-us">
+        <p class="abt-tlt">Why <span id="our">Choose</span> Us</p>
+        <div class="aboutMain-con">
+          <div class="abt-con">
+            <img
+              src="@/assets/images/shield-removebg-preview.png"
+              alt="trusted logo picture"
+            />
+            <h2>Trustworthy</h2>
+            <p>
+              GIGvalley platform is securely optimized, this makes our services
+              dependable, well grounded for individuals, businesses and
+              corporate organizations.
+            </p>
+          </div>
+          <div class="abt-con">
+            <img
+              src="@/assets/images/clock-removebg-preview.png"
+              alt="data Availability image"
+            />
+            <h2>24/7 Availability</h2>
+            <p>
+              We are always ready to meet high expectations and quality
+              standards. We are neither open nor close,we are always available
+              for you.
+            </p>
+          </div>
+          <div class="abt-con">
+            <img
+              src="@/assets/images/agent-removebg-preview.png"
+              alt="custormer service provider image"
+            />
+            <h2>Customer Support</h2>
+            <p>
+              Our team has enough and well experienced expertise whose priority
+              is to put our customers at the zenith. You speak, We listen and
+              Act.
+            </p>
+          </div>
+        </div>
+      </section>
+</template>
+
+<style scoped>
+#about-us {
+  /* border: 1px solid red; */
+  padding-bottom: 10em;
+  margin-top: 2em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.abt-tlt {
+  margin-top: 5vh;
+  font-size: 25px;
+  font-weight: 900;
+  font-weight: bolder;
+  text-align: center;
+}
+.aboutMain-con {
+  display: flex;
+  justify-content: space-around;
+  width: 90%;
+  padding: 30px 5em;
+  /* box-shadow: 0 0 7px rgb(214, 212, 212);  */
+  border-radius: 1em;
+  margin-top: 2em;
+}
+.abt-con {
+  height: 20em;
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 0.6em;
+  color: var(--primary-color);
+  background-color: #fdf7ff;
+  font-weight: bolder;
+}
+.abt-con:nth-child(1) {
+  background-color: rgba(255, 192, 203, 0.062);
+  box-shadow: 0 0 7px rgb(214, 212, 212);
+}
+.abt-con:nth-child(2) {
+  background-color: var(--primary-color);
+  color: var(--secondary-color);
+}
+.abt-con:nth-child(3) {
+  background-color: rgba(255, 192, 203, 0.062);
+  box-shadow: 0 0 7px rgb(214, 212, 212);
+}
+.abt-con img {
+  height: 30%;
+}
+.abt-con h2 {
+  margin-top: 2em;
+}
+
+.abt-con p {
+  font-weight: 100;
+  font-size: 13px;
+  text-align: center;
+  margin-top: 1em;
+}
+</style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,97 @@
+<template>
+ <section id="Hero">
+        <div class="hero-txt1">
+          <h1>
+            Low-priced <span class="type"></span> <br />
+            is What We give!
+          </h1>
+          <p class="lorem-text hero-lorem">
+            A no-frills 24/7/365 day per year Airtime Recharge l Data Bundles l
+            Cable Tv Subscription tailored for YOU.
+          </p>
+          <div class="hero-btn">
+            <button class="buy">Get Started</button>
+            <button class="learn">Explore</button>
+          </div>
+        </div>
+
+        <div class="worked-pics">
+          <img
+            src="@/assets/images/heroImg.jpg"
+            alt="data plan images logo"
+            class="photoshop-edit"
+          />
+        </div>
+      </section>
+</template>
+
+<style scoped>
+#Hero {
+  display: flex;
+}
+
+.worked-pics {
+  width: 50%;
+  padding: 10px;
+  /* border: 2px solid red;  */
+}
+.photoshop-edit {
+  width: 100%;
+}
+
+.hero-btn {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 2em;
+  width: 80%;
+  margin-top: 3em;
+}
+.buy:hover {
+  cursor: pointer;
+  background-color: var(--secondary-color);
+  color: var(--primary-color);
+  transition: 0.5s;
+  border: 1px solid var(--primary-color);
+}
+.buy {
+  background-color: var(--primary-color);
+  color: var(--secondary-color);
+  border: none;
+  width: 40%;
+  padding: 10px 0;
+  font-size: clamp(2px, 2vw, 15px);
+  outline: none;
+}
+.learn {
+  background-color: var(--secondary-color);
+  border: 1px solid var(--primary-color);
+  color: var(--primary-color);
+  width: 40%;
+  padding: 10px 0;
+  font-size: clamp(2px, 2vw, 15px);
+}
+.learn:hover {
+  cursor: pointer;
+  background-color: var(--primary-color);
+  color: var(--secondary-color);
+  transition: 0.5s;
+}
+.hero-btn button {
+  border-radius: 7px;
+}
+.hero-txt1 {
+  width: 50%;
+  flex-direction: column;
+}
+.hero-txt1 h1 {
+  font-weight: bolder;
+  font-weight: 900;
+  font-family: "inter";
+  font-size: clamp(0.9em, 5vw, 30em);
+}
+.hero-txt1 span {
+  color: var(--primary-color);
+}
+
+</style>
