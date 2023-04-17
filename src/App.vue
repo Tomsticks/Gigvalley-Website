0c6275a4c000ae26ecd05745952339808a3344b6
olowe temiloluwa <abosedeolowe2@gmail.com>
2023-04-17 05:39:21 -0700

    u

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
    
- <RouterView/>
-</template>
-<script setup>
-
-import {RouterView} from 'vue-router'
-</script>
-
-<style>
-
-</style>
+    <RouterView/>
+   </template>
+   <script setup>
+   import Login from './Pages/Register.vue';
+   
+   import {RouterView} from 'vue-router'
+   </script>
+   
+   <style>
+   
+   </style>
