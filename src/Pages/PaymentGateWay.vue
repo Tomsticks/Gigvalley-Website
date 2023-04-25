2d5f6353c49206b374bf8c0fa1e194608022f81d
Tomsticks <miracleolaniyan@yahoo.com>
2023-04-25 12:59:39 +0100

    hj

--- src/Pages/PaymentGateWay.vue
+++ src/Pages/PaymentGateWay.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="grid place-items-center h-[100vh] bg-[#eee]">
-    <div class="sm:w-full sm:h-full md:w-[80%] lg:w-[65%] sm:rounded-none flex flex-col w-[80vh] h-[70%] rounded-xl justify-center items-center bg-primary">
-    <h1 class="mb-7 capitalize text-white font-semibold text-2xl">
+    <div class="sm:w-full sm:h-full md:w-[80%] lg:w-[65%] sm:rounded-none py- flex flex-col w-[80vh] h-[70%] rounded-xl justify-center gap-4 items-center bg-primary">
+    <h1 class="capitalize text-white font-semibold text-2xl">
       select a payment method
     </h1>
-    <div class="flex flex-col gap-6 mt-5 w-full items-center  ">
+    <div class="flex flex-col gap-6  w-full items-center  ">
       <button class="flex items-center justify-between px-6 py-2 bg-white text-primary w-[70%] h-35 text-lg rounded-lg font-semibold capitalize active:shadow-inner active:shadow-black hover:">payment gateway<img   class="w-8 h-8"  src="../assets/images/paymentt.png" alt=""></button>
       <button class="flex items-center justify-between px-6 py-2 bg-white text-primary w-[70%] h-35 text-lg rounded-lg font-semibold capitalize active:shadow-inner active:shadow-black hover:"><p>automated funding</p><svg  class="w-8 h-8" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
   <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z"></path>
